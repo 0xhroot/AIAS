a373226789d4f00a07007bdd2003e2ec93e6a3dc
0xhroot <91869614+0xhroot@users.noreply.github.com>
2025-09-17 00:04:09 +0530

    Delete utils directory

--- utils/scapy_patch.py
+++
@@ -1,6 +0,0 @@
-import sys, types, six
-
-# Create fake scapy.modules.six.moves
-six_moves = types.ModuleType("scapy.modules.six.moves")
-six_moves.range = six.moves.range
-sys.modules["scapy.modules.six.moves"] = six_moves
